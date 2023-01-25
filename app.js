3d3b6ea30622a10c841c5d83a2d943617f76e135
Rodrigo Lezana <75607037+kunx1@users.noreply.github.com>
2023-01-25 20:44:58 -0300

    se baja la imagen

---
+++ app.js
@@ -0,0 +1,36 @@
+const button = document.querySelector('button')
+const nav = document.querySelector('nav')
+
+button.addEventListener('click', ()=>{
+nav.classList.toggle('activo')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.addEventListener('scroll', function() {
+//     let header = document.querySelector('header');
+//     if (window.scrollY > 0) {
+//         header.classList.add('scrolled');
+//     } else {
+//         header.classList.remove('scrolled');
+//     }
+// });
